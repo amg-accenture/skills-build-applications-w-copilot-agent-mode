08c9e9ecbb01f9a2adab3cf1ad8b4129f4aaf13f
amg-accenture <juan.a.martin.gorriz@accenture.com>
2026-04-22 11:05:18 +0000

    prueba 10

--- octofit-tracker/frontend/src/components/Users.js
+++ octofit-tracker/frontend/src/components/Users.js
@@ -9,8 +9,8 @@ const Users = () => {
     const fetchUsers = async () => {
       try {
 
- const apiUrl = "/api/users/";
- const response = await fetch(apiUrl);
+ const apiUrl =
+    "https://animated-space-sniffle-774vv9g6vwwfxxxq-8000.app.github.dev/api/users/";
         console.log('Fetching users from:', apiUrl);
        // console.log('Using codespace URL:', !!codespaceName);
 
