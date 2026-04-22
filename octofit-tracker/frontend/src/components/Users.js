fbe3b6c75192abcd03882a72d4a8f2e81003d4be
amg-accenture <juan.a.martin.gorriz@accenture.com>
2026-04-22 10:00:21 +0000

    prueba cambio const users

--- octofit-tracker/frontend/src/components/Users.js
+++ octofit-tracker/frontend/src/components/Users.js
@@ -8,12 +8,7 @@ const Users = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const codespaceName = process.env.REACT_APP_CODESPACE_NAME;
-        // In codespace environment, always use codespace URL
-        const baseUrl = codespaceName
-          ? `https://${codespaceName}-8000.app.github.dev`
-          : 'http://localhost:8000';
-        const apiUrl = `${baseUrl}/api/users/`;
+const apiUrl = "https://animated-space-sniffle-774vv9g6vwwfxxxq-8000.app.github.dev/api/users/";
 
         console.log('Fetching users from:', apiUrl);
         console.log('Using codespace URL:', !!codespaceName);
